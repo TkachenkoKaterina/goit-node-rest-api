8911fd6fdb76b619a3847f47a1cbe7b7f00f5825
TkachenkoKaterina <tkachenko.city@gmail.com>
2024-05-09 21:06:53 +0300

    step 2 done

---
+++ services/contactsServices.js
@@ -0,0 +1,41 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+const updateContact = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContact(contacts);
+  return result;
+};
+
+export const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await updateContact(contacts);
+  return newContact;
+};
